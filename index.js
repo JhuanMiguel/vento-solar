867a105974f617ad5b850768921ef46e17f00f1b
Juan Miguel Oliveira Roque Bezerra <65321923+JhuanMiguel@users.noreply.github.com>
2022-03-29 11:12:08 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let menuToggler = document.querySelector(".mobile-menu");
+let menu = document.querySelector(".nav");
+let navLinks = document.querySelectorAll(".nav-links");
+let header = document.querySelector(".header");
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 0) header.classList.add('scroll-change'); // > 0 ou outro valor desejado
+  else header.classList.remove('scroll-change');
+});
+
+function animateMenu() {
+  navLinks.forEach((link, index) => {
+    link.style.animation
+      ? (link.style.animation = "")
+      : (link.style.animation = `LinksFadeIn 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`);
+  });
+}
+
+menuToggler.addEventListener("click", function () {
+  menu.classList.toggle("active");
+  menuToggler.classList.toggle("active");
+  animateMenu();
+});
